8617f0d3ad45e68180d1c336501faffbaa348554
KS <soni.karan.h@gmail.com>
2026-04-09 20:25:37 -0400

    chatbot.py

--- langchain_prompts/chatbot.py
+++ langchain_prompts/chatbot.py
@@ -2,11 +2,15 @@ from langchain_anthropic import ChatAnthropic
 from dotenv import load_dotenv
 
 load_dotenv()
-model = ChatAnthropic() 
-
+model = ChatAnthropic(model_name="claude-sonnet-4-6")
+chat_history = []
 while True:
     user_input = input("You: ")
+    chat_history.append({"role": "user", "content": user_input})
     if user_input.lower() == "exit":
         break
-    response = model.invoke(user_input)
+    response = model.invoke(chat_history)
+    chat_history.append({"role": "assistant", "content": response.content})
     print(f"Bot: {response.content}")
+
+print(chat_history)
